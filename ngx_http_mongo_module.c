9d5540b5259e6723f409d158568900da2f8cd819
wiliam <mellamowiliam@gmail.com>
2013-02-09 13:29:12 +0100

    First commit with module struct.

---
+++ ngx_http_mongo_module.c
@@ -0,0 +1,13 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Wiliam A. Rodrigo
+ */
+
+#define NGX_ESCAPE_REDIS   4
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <nginx.h>
